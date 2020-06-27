48e470c19832db936b78fb56f2dcf3d18eaa5670
1292460369@qq.com <a310553455>
2020-06-27 19:53:02 +0800

    修改了详情页面

--- pages/diary/index.vue
+++ pages/diary/index.vue
@@ -1,28 +1,123 @@
 <template>
-	<view class="container">
-		<view class="intro">本项目已包含uni ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。</view>
-		<text class="intro">详见：</text>
-		<uni-link :href="href" :text="href"></uni-link>
-	</view>
+    <view>
+        <view class="article-meta">
+            <text class="article-author">{{banner.author_name}}</text>
+            <text class="article-text">发表于</text>
+            <text class="article-time">{{banner.published_at}}</text>
+        </view>
+        <view class="article-content">
+            <rich-text :nodes="htmlString"></rich-text>
+        </view>
+        <!-- #ifdef MP-WEIXIN -->
+		<!-- 这里是小程序的广告位 -->
+        <!-- <ad v-if="htmlString" unit-id="adunit-01b7a010bf53d74e"></ad> -->
+        <!-- #endif -->
+    </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
-			}
-		},
-		methods: {
+    export default {
+        data() {
+            return {
+                banner: {
+					author_name:'zzzz',
+					published_at:'2018.1.1'
+				},
+                htmlString: "评价列表，传入数据即可，具体参考Demo，修复当评价内容过长，样式显示错乱问题,修改示例图片评价列表，传入数据即可，具体参考Demo，修复当评价内容过长，样式显示错乱问题,修改示例图片评价列表，传入数据即可，具体参考Demo，修复当评价内容过长，样式显示错乱问题,修改示例图片评价列表，传入数据即可，具体参考Demo，修复当评价内容过长，样式显示错乱问题,修改示例图片评价列表，传入数据即可，具体参考Demo，修复当评价内容过长，样式显示错乱问题,修改示例图片评价列表，传入数据即可，具体参考Demo，修复当评价内容过长，样式显示错乱问题,修改示例图片评价列表，传入数据即可，具体参考Demo，修复当评价内容过长，样式显示错乱问题,修改示例图片评价列表，传入数据即可，具体参考Demo，修复当评价内容过长，样式显示错乱问题,修改示例图片评价列表，传入数据即可，具体参考Demo，修复当评价内容过长，样式显示错乱问题,修改示例图片评价列表，传入数据即可，具体参考Demo，修复当评价内容过长，样式显示错乱问题,修改示例图片评价列表，传入数据即可，具体参考Demo，修复当评价内容过长，样式显示错乱问题,修改示例图片评价列表，传入数据即可，具体参考Demo，修复当评价内容过长，样式显示错乱问题,修改示例图片评价列表，传入数据即可，具体参考Demo，修复当评价内容过长，样式显示错乱问题,修改示例图片评价列表，传入数据即可，具体参考Demo，修复当评价内容过长，样式显示错乱问题,修改示例图片评价列表，传入数据即可，具体参考Demo，修复当评价内容过长，样式显示错乱问题,修改示例图片"
+            }
+        },
+        onShareAppMessage() {
+            return {
+                title: this.banner.title,
+                path: '/pages/template/list2detail-detail/list2detail-detail?detailDate=' + JSON.stringify(this.banner)
+            }
+        },
+        onLoad(event) {
+            // 目前在某些平台参数会被主动 decode，暂时这样处理。
+            // try {
+            //     this.banner = JSON.parse(decodeURIComponent(event.detailDate));
+            // } catch (error) {
+            //     this.banner = JSON.parse(event.detailDate);
+            // }
 
-		}
-	}
+            // //this.getDetail();
+            // uni.setNavigationBarTitle({
+            //     title: this.banner.title
+            // });
+        },
+        methods: {
+            getDetail() {
+                uni.request({
+                    url: 'https://unidemo.dcloud.net.cn/api/news/36kr/' + this.banner.post_id,
+                    success: (data) => {
+                        if (data.statusCode == 200) {
+                            this.htmlString = data.data.content.replace(/\\/g, "").replace(/<img/g,
+                                "<img style=\"display:none;\"");
+                        }
+                    },
+                    fail: () => {
+                        console.log('fail');
+                    }
+                })
+            }
+        }
+    }
 </script>
 
 <style>
-	.container {
-		padding: 20px;
-		font-size: 14px;
-		line-height: 24px;
-	}
+    view {
+        font-size: 28upx;
+        line-height: 1.8;
+    }
+
+    .banner {
+        height: 360upx;
+        overflow: hidden;
+        position: relative;
+        background-color: #ccc;
+    }
+
+    .banner-img {
+        width: 100%;
+    }
+
+    .banner-title {
+        max-height: 84upx;
+        overflow: hidden;
+        position: absolute;
+        left: 30upx;
+        bottom: 30upx;
+        width: 90%;
+        font-size: 32upx;
+        font-weight: 400;
+        line-height: 42upx;
+        color: white;
+        z-index: 11;
+    }
+
+    .article-meta {
+        padding: 20upx 40upx;
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        color: gray;
+    }
+
+    .article-text {
+        font-size: 26upx;
+        line-height: 50upx;
+        margin: 0 20upx;
+    }
+
+    .article-author,
+    .article-time {
+        font-size: 30upx;
+    }
+
+    .article-content {
+        padding: 0 30upx;
+        overflow: hidden;
+        font-size: 30upx;
+        margin-bottom: 30upx;
+    }
 </style>
--- unpackage/dist/dev/mp-weixin/common/vendor.js
+++ unpackage/dist/dev/mp-weixin/common/vendor.js
@@ -1693,7 +1693,7 @@ function normalizeComponent (
 
 /***/ }),
 
-/***/ 116:
+/***/ 117:
 /*!*********************************************************************!*\
   !*** /Users/macbook/html/xiuxiudiary/components/uni-icons/icons.js ***!
   \*********************************************************************/
@@ -1832,28 +1832,6 @@ Object.defineProperty(exports, "__esModule", { value: true });exports.default = 
   "cloud-download-filled": "\uE8E9",
   "headphones": "\uE8BF",
   "shop": "\uE609" };exports.default = _default;
-
-/***/ }),
-
-/***/ 165:
-/*!*****************************************************************!*\
-  !*** /Users/macbook/html/xiuxiudiary/pages/tabBar/diary/api.js ***!
-  \*****************************************************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });exports.diaryList = void 0;var http = _interopRequireWildcard(__webpack_require__(/*! ../../../utils/service.js */ 42));function _getRequireWildcardCache() {if (typeof WeakMap !== "function") return null;var cache = new WeakMap();_getRequireWildcardCache = function _getRequireWildcardCache() {return cache;};return cache;}function _interopRequireWildcard(obj) {if (obj && obj.__esModule) {return obj;}if (obj === null || typeof obj !== "object" && typeof obj !== "function") {return { default: obj };}var cache = _getRequireWildcardCache();if (cache && cache.has(obj)) {return cache.get(obj);}var newObj = {};var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor;for (var key in obj) {if (Object.prototype.hasOwnProperty.call(obj, key)) {var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null;if (desc && (desc.get || desc.set)) {Object.defineProperty(newObj, key, desc);} else {newObj[key] = obj[key];}}}newObj.default = obj;if (cache) {cache.set(obj, newObj);}return newObj;}
-
-var diaryList = function diaryList(data) {
-  return new Promise(function (resolve, reject) {
-    http.get('diaryList', data).then(function (res) {
-      resolve(res);
-    }).catch(function (err) {
-      reject(err);
-    });
-  });
-};exports.diaryList = diaryList;
 
 /***/ }),
 
@@ -7887,71 +7865,29 @@ internalMixin(Vue);
 
 /***/ }),
 
-/***/ 3:
-/*!***********************************!*\
-  !*** (webpack)/buildin/global.js ***!
-  \***********************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-var g;
-
-// This works in non-strict mode
-g = (function() {
-	return this;
-})();
-
-try {
-	// This works if eval is allowed (see CSP)
-	g = g || new Function("return this")();
-} catch (e) {
-	// This works if the window reference is available
-	if (typeof window === "object") g = window;
-}
-
-// g can still be undefined, but nothing to do about it...
-// We return undefined, instead of nothing here, so it's
-// easier to handle this case. if(!global) { ...}
-
-module.exports = g;
-
-
-/***/ }),
-
-/***/ 4:
-/*!**************************************************!*\
-  !*** /Users/macbook/html/xiuxiudiary/pages.json ***!
-  \**************************************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-
-
-/***/ }),
-
-/***/ 41:
-/*!**********************************************************!*\
-  !*** /Users/macbook/html/xiuxiudiary/pages/diary/api.js ***!
-  \**********************************************************/
+/***/ 25:
+/*!*****************************************************************!*\
+  !*** /Users/macbook/html/xiuxiudiary/pages/tabBar/diary/api.js ***!
+  \*****************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });exports.diaryAdd = void 0;var http = _interopRequireWildcard(__webpack_require__(/*! ../../utils/service.js */ 42));function _getRequireWildcardCache() {if (typeof WeakMap !== "function") return null;var cache = new WeakMap();_getRequireWildcardCache = function _getRequireWildcardCache() {return cache;};return cache;}function _interopRequireWildcard(obj) {if (obj && obj.__esModule) {return obj;}if (obj === null || typeof obj !== "object" && typeof obj !== "function") {return { default: obj };}var cache = _getRequireWildcardCache();if (cache && cache.has(obj)) {return cache.get(obj);}var newObj = {};var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor;for (var key in obj) {if (Object.prototype.hasOwnProperty.call(obj, key)) {var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null;if (desc && (desc.get || desc.set)) {Object.defineProperty(newObj, key, desc);} else {newObj[key] = obj[key];}}}newObj.default = obj;if (cache) {cache.set(obj, newObj);}return newObj;}
+Object.defineProperty(exports, "__esModule", { value: true });exports.diaryList = void 0;var http = _interopRequireWildcard(__webpack_require__(/*! ../../../utils/service.js */ 26));function _getRequireWildcardCache() {if (typeof WeakMap !== "function") return null;var cache = new WeakMap();_getRequireWildcardCache = function _getRequireWildcardCache() {return cache;};return cache;}function _interopRequireWildcard(obj) {if (obj && obj.__esModule) {return obj;}if (obj === null || typeof obj !== "object" && typeof obj !== "function") {return { default: obj };}var cache = _getRequireWildcardCache();if (cache && cache.has(obj)) {return cache.get(obj);}var newObj = {};var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor;for (var key in obj) {if (Object.prototype.hasOwnProperty.call(obj, key)) {var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null;if (desc && (desc.get || desc.set)) {Object.defineProperty(newObj, key, desc);} else {newObj[key] = obj[key];}}}newObj.default = obj;if (cache) {cache.set(obj, newObj);}return newObj;}
 
-var diaryAdd = function diaryAdd(data) {
+var diaryList = function diaryList(data) {
   return new Promise(function (resolve, reject) {
-    http.post('diaryAdd', data).then(function (res) {
+    http.get('diaryList', data).then(function (res) {
       resolve(res);
     }).catch(function (err) {
       reject(err);
     });
   });
-};exports.diaryAdd = diaryAdd;
+};exports.diaryList = diaryList;
 
 /***/ }),
 
-/***/ 42:
+/***/ 26:
 /*!********************************************************!*\
   !*** /Users/macbook/html/xiuxiudiary/utils/service.js ***!
   \********************************************************/
@@ -7959,7 +7895,7 @@ var diaryAdd = function diaryAdd(data) {
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-/* WEBPACK VAR INJECTION */(function(uni) {Object.defineProperty(exports, "__esModule", { value: true });exports.del = exports.put = exports.post = exports.get = void 0;var _regenerator = _interopRequireDefault(__webpack_require__(/*! ./node_modules/@vue/babel-preset-app/node_modules/@babel/runtime/regenerator */ 43));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {try {var info = gen[key](arg);var value = info.value;} catch (error) {reject(error);return;}if (info.done) {resolve(value);} else {Promise.resolve(value).then(_next, _throw);}}function _asyncToGenerator(fn) {return function () {var self = this,args = arguments;return new Promise(function (resolve, reject) {var gen = fn.apply(self, args);function _next(value) {asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);}function _throw(err) {asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);}_next(undefined);});};}var baseUrl = 'http://www.lqy.com/api/';
+/* WEBPACK VAR INJECTION */(function(uni) {Object.defineProperty(exports, "__esModule", { value: true });exports.del = exports.put = exports.post = exports.get = void 0;var _regenerator = _interopRequireDefault(__webpack_require__(/*! ./node_modules/@vue/babel-preset-app/node_modules/@babel/runtime/regenerator */ 27));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {try {var info = gen[key](arg);var value = info.value;} catch (error) {reject(error);return;}if (info.done) {resolve(value);} else {Promise.resolve(value).then(_next, _throw);}}function _asyncToGenerator(fn) {return function () {var self = this,args = arguments;return new Promise(function (resolve, reject) {var gen = fn.apply(self, args);function _next(value) {asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);}function _throw(err) {asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);}_next(undefined);});};}var baseUrl = 'http://www.lqy.com/api/';
 // let baseUrl = 'https://www.ergouphp.com/api/'
 
 var headers = {
@@ -8063,18 +7999,18 @@ var del = function del(url, data) {var headertype = arguments.length > 2 && argu
 
 /***/ }),
 
-/***/ 43:
+/***/ 27:
 /*!*********************************************************************************************!*\
   !*** ./node_modules/@vue/babel-preset-app/node_modules/@babel/runtime/regenerator/index.js ***!
   \*********************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! regenerator-runtime */ 44);
+module.exports = __webpack_require__(/*! regenerator-runtime */ 28);
 
 /***/ }),
 
-/***/ 44:
+/***/ 28:
 /*!************************************************************!*\
   !*** ./node_modules/regenerator-runtime/runtime-module.js ***!
   \************************************************************/
@@ -8105,7 +8041,7 @@ var oldRuntime = hadRuntime && g.regeneratorRuntime;
 // Force reevalutation of runtime.js.
 g.regeneratorRuntime = undefined;
 
-module.exports = __webpack_require__(/*! ./runtime */ 45);
+module.exports = __webpack_require__(/*! ./runtime */ 29);
 
 if (hadRuntime) {
   // Restore the original runtime.
@@ -8122,7 +8058,7 @@ if (hadRuntime) {
 
 /***/ }),
 
-/***/ 45:
+/***/ 29:
 /*!*****************************************************!*\
   !*** ./node_modules/regenerator-runtime/runtime.js ***!
   \*****************************************************/
@@ -8854,7 +8790,71 @@ if (hadRuntime) {
 
 /***/ }),
 
-/***/ 52:
+/***/ 3:
+/*!***********************************!*\
+  !*** (webpack)/buildin/global.js ***!
+  \***********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+var g;
+
+// This works in non-strict mode
+g = (function() {
+	return this;
+})();
+
+try {
+	// This works if eval is allowed (see CSP)
+	g = g || new Function("return this")();
+} catch (e) {
+	// This works if the window reference is available
+	if (typeof window === "object") g = window;
+}
+
+// g can still be undefined, but nothing to do about it...
+// We return undefined, instead of nothing here, so it's
+// easier to handle this case. if(!global) { ...}
+
+module.exports = g;
+
+
+/***/ }),
+
+/***/ 4:
+/*!**************************************************!*\
+  !*** /Users/macbook/html/xiuxiudiary/pages.json ***!
+  \**************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+
+
+/***/ }),
+
+/***/ 46:
+/*!**********************************************************!*\
+  !*** /Users/macbook/html/xiuxiudiary/pages/diary/api.js ***!
+  \**********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });exports.diaryAdd = void 0;var http = _interopRequireWildcard(__webpack_require__(/*! ../../utils/service.js */ 26));function _getRequireWildcardCache() {if (typeof WeakMap !== "function") return null;var cache = new WeakMap();_getRequireWildcardCache = function _getRequireWildcardCache() {return cache;};return cache;}function _interopRequireWildcard(obj) {if (obj && obj.__esModule) {return obj;}if (obj === null || typeof obj !== "object" && typeof obj !== "function") {return { default: obj };}var cache = _getRequireWildcardCache();if (cache && cache.has(obj)) {return cache.get(obj);}var newObj = {};var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor;for (var key in obj) {if (Object.prototype.hasOwnProperty.call(obj, key)) {var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null;if (desc && (desc.get || desc.set)) {Object.defineProperty(newObj, key, desc);} else {newObj[key] = obj[key];}}}newObj.default = obj;if (cache) {cache.set(obj, newObj);}return newObj;}
+
+var diaryAdd = function diaryAdd(data) {
+  return new Promise(function (resolve, reject) {
+    http.post('diaryAdd', data).then(function (res) {
+      resolve(res);
+    }).catch(function (err) {
+      reject(err);
+    });
+  });
+};exports.diaryAdd = diaryAdd;
+
+/***/ }),
+
+/***/ 53:
 /*!****************************************************************!*\
   !*** /Users/macbook/html/xiuxiudiary/pages/tabBar/user/api.js ***!
   \****************************************************************/
@@ -8862,7 +8862,7 @@ if (hadRuntime) {
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });exports.userCode = void 0;var http = _interopRequireWildcard(__webpack_require__(/*! ../../../utils/service.js */ 42));function _getRequireWildcardCache() {if (typeof WeakMap !== "function") return null;var cache = new WeakMap();_getRequireWildcardCache = function _getRequireWildcardCache() {return cache;};return cache;}function _interopRequireWildcard(obj) {if (obj && obj.__esModule) {return obj;}if (obj === null || typeof obj !== "object" && typeof obj !== "function") {return { default: obj };}var cache = _getRequireWildcardCache();if (cache && cache.has(obj)) {return cache.get(obj);}var newObj = {};var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor;for (var key in obj) {if (Object.prototype.hasOwnProperty.call(obj, key)) {var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null;if (desc && (desc.get || desc.set)) {Object.defineProperty(newObj, key, desc);} else {newObj[key] = obj[key];}}}newObj.default = obj;if (cache) {cache.set(obj, newObj);}return newObj;}
+Object.defineProperty(exports, "__esModule", { value: true });exports.userCode = void 0;var http = _interopRequireWildcard(__webpack_require__(/*! ../../../utils/service.js */ 26));function _getRequireWildcardCache() {if (typeof WeakMap !== "function") return null;var cache = new WeakMap();_getRequireWildcardCache = function _getRequireWildcardCache() {return cache;};return cache;}function _interopRequireWildcard(obj) {if (obj && obj.__esModule) {return obj;}if (obj === null || typeof obj !== "object" && typeof obj !== "function") {return { default: obj };}var cache = _getRequireWildcardCache();if (cache && cache.has(obj)) {return cache.get(obj);}var newObj = {};var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor;for (var key in obj) {if (Object.prototype.hasOwnProperty.call(obj, key)) {var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null;if (desc && (desc.get || desc.set)) {Object.defineProperty(newObj, key, desc);} else {newObj[key] = obj[key];}}}newObj.default = obj;if (cache) {cache.set(obj, newObj);}return newObj;}
 
 var userCode = function userCode(data) {
   return new Promise(function (resolve, reject) {
