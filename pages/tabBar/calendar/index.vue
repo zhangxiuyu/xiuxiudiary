d22c20c0cc9bad7e497fc3778ae88fc4c95e86f8
1292460369@qq.com <a310553455>
2021-01-29 10:48:21 +0800

    修复版本更新、日历显示问题

--- pages/tabBar/calendar/index.vue
+++ pages/tabBar/calendar/index.vue
@@ -4,46 +4,44 @@
 		<view class="uni-list">
 			<view class="uni-list-cell">
 				<view class="uni-list-cell-db">
-					<picker mode="date" :value="date"  @change="bindDateChange">
+					<picker mode="date" :value="date" @change="bindDateChange">
 						<view class="uni-input" style="margin-left: 25%; margin-top: 20rpx;">日期 ： {{date}}</view>
 					</picker>
 				</view>
 			</view>
 		</view>
-		
-		<!-- 日历 -->
-		<view class="warp">
-		        <view class="box">
-		            <t-table @change="change">
-		                <t-tr v-for="item in tableList" :key="item.id">
-		                    <t-td>{{ item.name }}</t-td>
-		                    <t-td>{{ item.hobby }}</t-td>
-		                </t-tr>
-		            </t-table>
-		        </view>
-		       
-		    </view>
-		
+
+
+		<!-- 你的图图小可爱写的 -->
+		<view class="content">
+			<view class="list">
+				<view class="item" v-for="item in tableList" :key="item.id">
+					<view class="left">{{ item.name }}</view>
+					<view class="right">{{ item.hobby }}</view>
+				</view>
+			</view>
+		</view>
+
 	</view>
 </template>
 
 <script>
-	 import tTable from '@/components/t-table/t-table.vue';
-	    import tTh from '@/components/t-table/t-th.vue';
-	    import tTr from '@/components/t-table/t-tr.vue';
-	    import tTd from '@/components/t-table/t-td.vue';
+	import tTable from '@/components/t-table/t-table.vue';
+	import tTh from '@/components/t-table/t-th.vue';
+	import tTr from '@/components/t-table/t-tr.vue';
+	import tTd from '@/components/t-table/t-td.vue';
 	export default {
-		 components: {
-		            tTable,
-		            tTh,
-		            tTr,
-		            tTd
-		        },
+		components: {
+			tTable,
+			tTh,
+			tTr,
+			tTd
+		},
 		data() {
 			const currentDate = this.getDate({
-			            format: true
-			        })
-					
+				format: true
+			})
+
 			return {
 				date: currentDate,
 				time: '12:01',
@@ -56,7 +54,7 @@
 				let year = date.getFullYear();
 				let month = date.getMonth() + 1;
 				let day = date.getDate();
-	
+
 				if (type === 'start') {
 					year = year - 60;
 				} else if (type === 'end') {
@@ -64,37 +62,69 @@
 				}
 				month = month > 9 ? month : '0' + month;;
 				day = day > 9 ? day : '0' + day;
-				this.getCalendar(year,month,day)
+				this.getCalendar(year, month, day)
 				return `${year}-${month}-${day}`;
 			},
 			bindDateChange: function(e) {
-			    this.date = e.target.value
+				this.date = e.target.value
 				const str = this.date.split('-')
-				this.getCalendar(str[0],str[1],str[2])
+				this.getCalendar(str[0], str[1], str[2])
 			},
-			getCalendar(year,month,day){
+			getCalendar(year, month, day) {
 				this.$http
-					.get({
-						url: "getCalendar",
-						data: {
-							year:year,
-							month:month,
-							day:day,
-						},
-					})
-					.then((res) => {
-						this.tableList = res.data
-					});
+						.get({
+							url: "getCalendar",
+							data: {
+								year: year,
+								month: month,
+								day: day,
+							},
+						})
+						.then((res) => {
+							this.tableList = res.data
+						});
 			}
 		}
 	}
 </script>
 
-<style>
-	.uni-column{
-		flex-direction:column
+<style lang="scss">
+	.uni-column {
+		flex-direction: column
 	}
-	.warp{
+
+	.warp {
 		margin-top: 30rpx;
+	}
+	.content{
+		.list{
+			.item{
+				display: flex;
+				color: #555c60;
+				font-size: 28rpx;
+				min-height: 70rpx;
+				border-bottom: 1rpx solid #d0dee5;
+				.left{
+					display: flex;
+					flex: 1;
+					align-items: center;
+					justify-content: center;
+					border-right:1rpx solid #d0dee5 ;
+				}
+				.right{
+					display: flex;
+					flex: 1;
+					align-items: center;
+					justify-content: center;
+					padding: 20rpx;
+				}
+			}
+			.item:nth-child(2n) {
+				background: #f5f5f5;
+			}
+			.item:first-child {
+				border-top: 1rpx solid #d0dee5;
+			}
+		}
 	}
 </style>
